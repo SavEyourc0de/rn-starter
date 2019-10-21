8b28a67e89648607bc5826c9b904ae767304c5dc
Xavier Johanis <xavierjohanis@gmail.com>
2019-10-21 16:51:17 +0800

    flatlist

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Text, View, StyleSheet, FlatList} from 'react-native';
+
+const ListScreen = () => {
+    const friends = [
+        { name: 'friend 1', age: Math.floor(Math.random() * 25) + 20},
+        { name: 'friend 2', age: Math.floor(Math.random() * 25) + 20},
+        { name: 'friend 3', age: Math.floor(Math.random() * 25) + 20},
+        { name: 'friend 4', age: Math.floor(Math.random() * 25) + 20},
+        { name: 'friend 5', age: Math.floor(Math.random() * 25) + 20},
+        { name: 'friend 6', age: Math.floor(Math.random() * 25) + 20},
+        { name: 'friend 7', age: Math.floor(Math.random() * 25) + 20},
+        { name: 'friend 8', age: Math.floor(Math.random() * 25) + 20},
+    ];
+    
+    return ( //flatlist: element === {item: {'friend #1}, index: 0}
+        <FlatList
+            showsVerticalScrollIndicator={false}
+            keyExtractor={friend => friend.name}
+            data={friends} 
+            renderItem={({item}) => {
+                return (
+                    <View style={styles.textStyle}>
+                        <Text style={styles.listText}>{item.name} - Age: {item.age}</Text>
+                    </View>
+                );
+            }}
+        />
+    );
+};
+
+const styles = StyleSheet.create({
+    textStyle: {
+        marginVertical: 50
+    },
+    listText: {
+        fontSize: 30
+    }
+});
+export default ListScreen;
