f37df64686076fc7bc31ddbaf3001e5460a65a8d
Xavier Johanis <xavierjohanis@gmail.com>
2019-10-21 15:31:20 +0800

    getting started

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,12 +2,20 @@ import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
 const ComponentsScreen = () => {
-    return <Text style={styles.textStyle}>This is components screen</Text>;
+    return (
+    <View>
+        <Text style={styles.textStyle}>Getting started with react native</Text>
+        <Text style={styles.subHeaderStyle}>This is components screen.</Text>
+    </View>
+    );
 };
 
 const styles = StyleSheet.create({
     textStyle: {
-        fontSize: 30
+        fontSize: 45
+    },
+    subHeaderStyle: {
+        fontSize: 20
     }
 });
 export default ComponentsScreen;
