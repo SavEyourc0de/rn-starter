cfd65876188e32f22e90f91e7aaf5526dfc2e98f
Xavier Johanis <xavierjohanis@gmail.com>
2019-10-24 20:25:54 +0800

    passing data using props

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,7 +4,6 @@ import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 const HomeScreen = ({navigation}) => {
   return (
     <View>
-      <Text style={styles.text}>Hi There!</Text>
       <Button
         onPress={() => navigation.navigate('Components')} 
         title={'Go to Components Demo'}
@@ -13,11 +12,19 @@ const HomeScreen = ({navigation}) => {
       <Button onPress={() => navigation.navigate('List')}
       title={'Go to List'}
       />
+
+      <Button
+        title={'To image Screen'}
+        onPress={() => navigation.navigate('Image')}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  button: {
+    width: 30
+  },
   text: {
     fontSize: 30
   }
